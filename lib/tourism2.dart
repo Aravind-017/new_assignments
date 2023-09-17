62ee7ce8ff28a7ef90c57c5237a9e680eee278e6
Aravind-017 <aravindshaji017@gmail.com>
2023-09-17 21:42:33 +0530

    initial commit

---
+++ lib/tourism2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class tour2 extends StatelessWidget {
+  const tour2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
